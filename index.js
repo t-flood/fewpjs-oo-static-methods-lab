5c230e964194af7375ed290e3ed82269f24ad34f
Tim Flood <tflood9@gmail.com>
2021-03-09 10:47:32 -0500

    commit tree fabbb79a02a5f7cd49497fff9e1d1803cdcd2a51

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-" "']+/g, "");
+  }
+
+  static titleize(string) {
+    let array = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    let newString = string.split(" ");
+    let finalString = [];
+    newString.forEach(function (word) {
+      if (array.includes(word)) {
+        if (finalString.length === 0) {
+          finalString.push(Formatter.capitalize(word));
+        } else {
+          finalString.push(word);
+        }
+      } else {
+        finalString.push(Formatter.capitalize(word));
+      }
+    });
+    return finalString.join(" ");
+  }
 }
